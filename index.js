da4c746a29c08c72450b6efc2ca8c579a1c15376
Dosh Barzanji <Doshbarzanji@gmail.com>
2026-02-09 20:06:43 -0700

    Remove nested folder

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
-require ('Datejs')
-
 require('datejs');
 
 function combineUsers(...args) {
-  const combinedObject = {
-    users: []
-  };
-  
-  for (let arr of args) {
-    combinedObject.users = [...combinedObject.users, ...arr];
+  const combinedObject = { users: [] };
+
+  for (const arr of args) {
+    combinedObject.users = combinedObject.users.concat(arr);
   }
-  
+
   combinedObject.merge_date = new Date().toString('M/d/yyyy');
-  
+
   return combinedObject;
 }
 
-module.exports = combineUsers;
-=======
-
-
-
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
-
+module.exports = { combineUsers };
