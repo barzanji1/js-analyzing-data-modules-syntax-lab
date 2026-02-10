3779befc5aaca01a1af78526f58ca63f850cad66
Dosh Barzanji <Doshbarzanji@gmail.com>
2026-02-09 19:12:32 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require ('Datejs')
+
+require('datejs');
+
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+  
+  for (let arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+  
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+  
+  return combinedObject;
+}
+
+module.exports = combineUsers;
